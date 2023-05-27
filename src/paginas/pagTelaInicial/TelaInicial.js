fa979873f3afd7785e2e05874aeda2676e703acc
ValterSoarees <valter.soareess@gmail.com>
2023-05-27 01:07:20 -0300

    iniciando back-end da página inicial

--- src/paginas/pagTelaInicial/TelaInicial.js
+++ src/paginas/pagTelaInicial/TelaInicial.js
@@ -1,75 +1,60 @@
-import { 
-  View, 
-  Text, 
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  ImageBackground, 
-  TextInput
-} from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ScrollView, Image } from 'react-native';
 
-import { useEffect, useState } from "react";
-import { useNavigation } from '@react-navigation/native';
+const LivroPage = () => {
+  const [livros, setlivros] = useState([]);
 
-const RelogioPageComponent = (props) => {
+  useEffect(() => {
+    const load = async () => {
+      const resultJson = await fetch(
+        'https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent("aventura")}&AIzaSyA2gnDV1nrAOHTaPOOCb181fexdxSXTc-c'
+      );
+      const resultProduct = await resultJson.json();
+      let resultFormatado = resultProduct.items.map(({ id, volumeInfo }) => ({
+        id: id,
+        nome: volumeInfo.title,
+        autor: volumeInfo.authors,
+        imagem: volumeInfo.imageLinks?.thumbnail,
+        categoria: volumeInfo.categories,
+        paginas: volumeInfo.pageCount,
+      }));
 
-    const [relogios, setRelogios] = useState([]);
-  
-    useEffect( () => {
-      const load = async () => {
-        const resultJson = await fetch("https://dummyjson.com/products/category/mens-watches");
-  
-        const resultProduct = await resultJson.json();
-        let resultFormatado = resultProduct.products.map(({ id, title, price, thumbnail, rating, stock }) => ({
-          id: id,
-          nome: title,
-          preco: price,
-          imagem: thumbnail,
-          avaliacao: rating,
-          estoque: stock,
-        }));
-  
-        setRelogios(resultFormatado);
-      };
-  
-      load();
-    },[] );
-  
-    return (
-          <>
-        <View>
-          <Text>Relógios</Text>
-  
-          <View>
-            {relogios.map((relogio) => {
-              return (
-                <View key={relogio.id}>
-                  <View>
-                    <Image
-                      src={relogio.imagem}
-                      />
-                  </View>
+      setlivros(resultFormatado);
+    };
+
+    load();
+  }, []);
+
+  return (
+    <>
+      <View>
+        <Text>Livros</Text>
+        <ScrollView>
+          {livros.map((livro) => {
+            return (
+              <View key={livro.id}>
+                <View>
+                  {livro.imagem && (
+                    <Image source={{ uri: livro.imagem }} style={{ width: 100, height: 100 }} />
+                  )}
+                </View>
+                <View>
+                  <Text>{livro.nome}</Text>
                   <View>
                     <View>
-                      {relogio.nome}
+                      <Text>{livro.autor}</Text>
+                      <Text>{livro.categoria}</Text>
+                      <Text>{livro.paginas}</Text>
                     </View>
-  
-                    <View>
-                      <View>{relogio.descricao}</View>
-                      <View>R${relogio.preco}</View>
-                      <View>{relogio.avaliacao}</View>
-                      <View>{relogio.estoque}</View>
-                    </View>
-
                   </View>
                 </View>
-              );
-            })}
-          </View>
-        </View>
-      </>
-    );
-  };
-  
-  export default RelogioPageComponent;
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+    </>
+  );
+};
+
+export default LivroPage;
